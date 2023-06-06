e92ea532a9e0e60d86595c0a1e887ecf02a60ea0
Desire <desire_guilarte@hotmail.com>
2023-06-06 11:34:21 -0400

    added python script to add info from ticketmaster api and add to my db

---
+++ FanAlarm/ClientApp/src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,27 @@
+﻿import React, { useState } from 'react';
+import TopArtists from '../TopArtists/TopArtists';
+import "./Dashboard.css";
+
+export default function Dashboard({ code }) {
+    const [token, setToken] = useState("")
+
+    const logout = () => {
+        setToken("")
+        window.localStorage.removeItem("token")
+    }
+
+    const CLIENT_ID = "197117536354511"
+    const REDIRECT_URI = "https://localhost:3000"
+    const AUTH_ENDPOINT = "https://api.instagram.com/oauth/authorize"
+    const RESPONSE_TYPE = "code"
+    const SCOPE = "user_profile,user_media"
+
+    return (<>
+                <div className='topArists-header'>
+                    <div className='topArtists-header-title'>Fan Alarm</div>
+                </div>
+                <TopArtists/>
+                <button onClick={logout}>Logout</button>
+        </>
+        );
+}
---
+++ FanAlarm/ClientApp/src/components/TopArtists/TopArtists.jsx
@@ -0,0 +1,57 @@
+﻿import axios from "axios";
+import React, { useEffect } from "react";
+import { reducerCases } from "../../utils/Constants";
+import { useStateProvider } from "../../utils/StateProvider";
+import "./TopArtists.css";
+import Grid from '@mui/material/Grid';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+export default function TopArtists() {
+    const [{ token, allArtists }, dispatch] = useStateProvider();
+    useEffect(() => {
+        const getArtistData = async () => {
+            const response = await axios.get(
+                "https://api.spotify.com/v1/me/top/artists",
+                {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+            const { items } = response.data;
+            console.log(items);
+            let counter = 1 ;
+            const allArtists = items.map(({ name, id }) => {
+                let totalArtists = counter ++ ;
+                return { name, id, totalArtists };
+            });
+            dispatch({ type: reducerCases.GET_ALL_ARTISTS, allArtists });
+        };
+        getArtistData();
+    }, [token, dispatch]);
+    return (
+        <div className="topArtists-wrapper">
+            <div className="topArtists-title">YOUR TOP ARTISTS</div>
+            <ul className="topArtists-lists">
+                {allArtists?.map(({ name, id, totalArtists }) => {
+                    return (
+                        <li className="topArtists-artist" key={id}>
+                            <Grid container spacing={3} sx={{ flexGrow: 1 }}>
+                                <Grid item xs={6} xsoffset={3} md={2} mdoffset={0}>
+                                    <div className="topArtists-artist-num">{totalArtists}</div> 
+                                </Grid>
+                                <Grid item xs={6} xsoffset={3} md={9} mdoffset={0}>
+                                    <div className="topArtists-artist-name">{name}</div>
+                                </Grid>
+                                <Grid item xs={6} xsoffset={3} md={1} mdoffset="auto">
+                                    <MoreHorizIcon className="topArtists-artist-more" />
+                                </Grid>
+                            </Grid>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+    );
+}
