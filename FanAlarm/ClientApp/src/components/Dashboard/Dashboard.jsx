dda3a9e6fb836f76fc8dbf217f690c51fd518dec
Desire <desire_guilarte@hotmail.com>
2025-07-13 22:14:19 +0800

    token issues

--- FanAlarm/ClientApp/src/components/Dashboard/Dashboard.jsx
+++ FanAlarm/ClientApp/src/components/Dashboard/Dashboard.jsx
@@ -3,11 +3,19 @@ import TopArtists from '../TopArtists/TopArtists';
 import "./Dashboard.css";
 
 export default function Dashboard({ code }) {
+    const hash = window.location.hash.substring(1);
+    const params = new URLSearchParams(hash);
+    const token = params.get("access_token");
+
+    // Optional: Remove the token from the URL after extraction
+    window.history.replaceState(null, null, window.location.pathname);
+
     const [token, setToken] = useState("")
 
     const logout = () => {
         setToken("")
         window.localStorage.removeItem("token")
+
     }
 
     return (<>
